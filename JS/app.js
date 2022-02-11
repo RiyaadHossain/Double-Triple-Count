ef6b898ebeea14add2c72072621b81f113152461
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-11 12:29:34 +0600

    Negative Integer input handled

--- JS/app.js
+++ JS/app.js
@@ -2,24 +2,29 @@
 const input_Field = document.getElementById("input-digit");
 const dobule_Btn = document.getElementById("dobule-btn");
 const triple_Btn = document.getElementById("triple-btn");
+const reset_Btn = document.getElementById("reset-btn");
 const display_Digit = document.getElementById("display-digit");
 
 // Dobule Button 
-document.getElementById("dobule-btn").addEventListener("click", () => {
-    if(typeof parseInt(input_Field.value) !== "number"){
-        alert("Please Input an Intiger")
-    }else{
-        display_Digit.innerText = parseFloat(input_Field.value) * 2;
+document.getElementById("dobule-btn").addEventListener("click", (e) => {
+    if (parseInt(input_Field.value) < 0) {
+      alert("Please Input a Positive Intiger");
+    } else {
+      display_Digit.innerText = parseFloat(input_Field.value) * 2;
     }
     input_Field.value = "";
 })
 
 // Triple Button 
 document.getElementById("triple-btn").addEventListener("click", () => {
-     if (typeof parseInt(input_Field.value) !== "number") {
-       alert("Please Input an Intiger");
+     if ( parseInt(input_Field.value) < 0) {
+       alert("Please Input a Positive Intiger");
      } else {
        display_Digit.innerText = parseFloat(input_Field.value) * 3;
      }
      input_Field.value = "";
 });
+
+document.getElementById("reset-btn").addEventListener("click", () => {
+    display_Digit.innerText = "00" ;
+})
